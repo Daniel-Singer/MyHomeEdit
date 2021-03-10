d68967e344f591279c1480bd5b76b003bca070b2
Daniel Singer <singer.daniel85@gmail.com>
2021-03-10 09:23:07 +0100

    bug fix for dragging position of element. Leftover artefact from developing script caused unexpected shift in position when dragging started. artefact removed

--- drag.js
+++ drag.js
@@ -1,102 +1,101 @@
 // dragging functions
 
 const Drag = (() => {
-  
-  function updatePosition(element, x, offsetX, y, offsetY) {
-    const menuWidth = document.querySelector('.w3-bar-block').getBoundingClientRect().width;
-    if (element !== null) {
-		var newx = x - offsetX - menuWidth - 40;
-		var newy = y - offsetY;
-		newx = Math.round(newx/5)*5;
-		newy = Math.round(newy/5)*5;
-		
-		element.setAttribute('transform', 'translate('+ newx + ',' + newy + ")");
-    }
-  };
 
-  function getOffset(measurement, offset, clientPosition) {
-    return measurement - (measurement + offset - clientPosition);
-  };
+	function updatePosition(element, x, offsetX, y, offsetY) {
+		const menuWidth = document.querySelector('.w3-bar-block').getBoundingClientRect().width;
+		console.log(menuWidth)
+		if (element !== null) {
+			var newx = x - offsetX - menuWidth;
+			var newy = y - offsetY;
+			newx = Math.round(newx / 5) * 5;
+			newy = Math.round(newy / 5) * 5;
 
-  return {
-    dragNode(e) {
-      let nodeContainer = null;
-      // find node container
-    //  nodeContainer = e.target.parentElement.parentElement;
-	  nodeContainer = e.target.parentElement;
-	  if (!nodeContainer.classList.contains('node-container')) {
-		  nodeContainer = nodeContainer.parentElement;
-	  }
-      if (nodeContainer.classList.contains('node-container')) {
-		
-        const rect = nodeContainer.getBoundingClientRect();
-
-        const offsetX = getOffset(rect.width, rect.x, e.clientX);
-        const offsetY = getOffset(rect.height, rect.y, e.clientY);
-
-        document.addEventListener('mousemove', function (e) {
-          updatePosition(nodeContainer, e.clientX, offsetX, e.clientY, offsetY)
-        });
-
-        document.addEventListener('mouseup', function () {
-          nodeContainer = null;
-        });
-		
-		var button = e.which || e.button;
-		var del = false;
-		if ( button === 3 ) {
-      		del = true;
-    	}
-		var id = nodeContainer.getAttribute('id');
-		
-		if(id.startsWith('fb_')) {
-		  var elem = actRoomController.roomm.FBArray[id];
-		  if(del) {
-			  // delete FB
-			  elem.delete();
-			  delete actRoomController.roomm.FBArray[id];
-		  }
-		  else {
-			properties.setFBProperties(elem);
-		  }
-		} else if(id.startsWith('func_')) {
-		  var elem = actRoomController.roomm.FuncArray[id];
-		  if(del) {
-			  // delete Func
-			  elem.delete();
-			  delete actRoomController.roomm.FuncArray[id];
-		  }
-		  else {
-			properties.setFuncProperties(elem);
-		  }
-		} else if(id.startsWith('in_')) {
-		  var elem = actRoomController.roomm.InArray[id];
-		  if(del) {
-			  // delete input
-			  elem.delete();
-			  delete actRoomController.roomm.InArray[id];
-		  }
-		  else {
-			properties.setInputProperties(elem);
-		  }
-		} else if(id.startsWith('out_')) {
-		  var elem = actRoomController.roomm.OutArray[id];
-		  if(del) {
-			  // delete Output
-			  elem.delete();
-			  delete actRoomController.roomm.OutArray[id];
-		  }
-		  else {
-			properties.setOutputProperties(elem);
-		  }
+			element.setAttribute('transform', 'translate(' + newx + ',' + newy + ")");
 		}
-      } else {
-		  // create connector
-		  
-	  }
-    }
-  }
+	};
+
+	function getOffset(measurement, offset, clientPosition) {
+		return measurement - (measurement + offset - clientPosition);
+	};
+
+	return {
+		dragNode(e) {
+			let nodeContainer = null;
+			// find node container
+			nodeContainer = e.target.parentElement;
+			if (!nodeContainer.classList.contains('node-container')) {
+				nodeContainer = nodeContainer.parentElement;
+			}
+			if (nodeContainer.classList.contains('node-container')) {
+
+				const rect = nodeContainer.getBoundingClientRect();
+
+				const offsetX = getOffset(rect.width, rect.x, e.clientX);
+				const offsetY = getOffset(rect.height, rect.y, e.clientY);
+
+				document.addEventListener('mousemove', function (e) {
+					updatePosition(nodeContainer, e.clientX, offsetX, e.clientY, offsetY)
+				});
+
+				document.addEventListener('mouseup', function () {
+					nodeContainer = null;
+				});
+
+				var button = e.which || e.button;
+				var del = false;
+				if (button === 3) {
+					del = true;
+				}
+				var id = nodeContainer.getAttribute('id');
+
+				if (id.startsWith('fb_')) {
+					var elem = actRoomController.roomm.FBArray[id];
+					if (del) {
+						// delete FB
+						elem.delete();
+						delete actRoomController.roomm.FBArray[id];
+					}
+					else {
+						properties.setFBProperties(elem);
+					}
+				} else if (id.startsWith('func_')) {
+					var elem = actRoomController.roomm.FuncArray[id];
+					if (del) {
+						// delete Func
+						elem.delete();
+						delete actRoomController.roomm.FuncArray[id];
+					}
+					else {
+						properties.setFuncProperties(elem);
+					}
+				} else if (id.startsWith('in_')) {
+					var elem = actRoomController.roomm.InArray[id];
+					if (del) {
+						// delete input
+						elem.delete();
+						delete actRoomController.roomm.InArray[id];
+					}
+					else {
+						properties.setInputProperties(elem);
+					}
+				} else if (id.startsWith('out_')) {
+					var elem = actRoomController.roomm.OutArray[id];
+					if (del) {
+						// delete Output
+						elem.delete();
+						delete actRoomController.roomm.OutArray[id];
+					}
+					else {
+						properties.setOutputProperties(elem);
+					}
+				}
+			} else {
+				// create connector
+
+			}
+		}
+	}
 
 
 })();
-
